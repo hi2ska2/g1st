421288e4ddb1a8cbf9c9611a519cfd55e1ddccc2
Sung-Min Hong <smhong@gist.ac.kr>
2018-04-09 22:53:05 +0900

    Add main.cc

---
+++ SRC/main.cc
@@ -0,0 +1,13 @@
+////////////////////////////////////////////////
+// G-1st                                      //
+//                                            //
+// Copyright 2018 Sung-Min Hong               //
+//                                            //
+// Please see LICENSE for license information //
+////////////////////////////////////////////////
+
+// main.cc
+
+int main()
+{
+}
